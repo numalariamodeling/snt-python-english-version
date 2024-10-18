e34a4b0d6a54fa839d820259e486e518aaa6a358
mohamedsillahkanu <114839447+mohamedsillahkanu@users.noreply.github.com>
2024-10-18 10:56:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -45,9 +45,9 @@ As SNT matures, more quality assurance is needed such that NMCPs can be confiden
             <p><em>This section explains the workflow of importing and managing shapefiles using Python.</em></p>
 
             <div class="round-buttons">
-                <button class="rect-button" onclick="window.location.href='https://';">View py EN</button>
-                <button class="rect-button" onclick="window.location.href='https://';">View R FR</button>
-                <button class="rect-button" onclick="window.location.href='https://';">View py FR</button>
+                <button class="rect-button" onclick="window.location.href='https://numalariamodeling.github.io/snt-code-library-english-version/#shapefiles';">View R En</button>
+                <button class="rect-button" onclick="window.location.href='https://numalariamodeling.github.io/snt-code-library-french-version/#shapefiles';">View R FR</button>
+                <button class="rect-button" onclick="window.location.href='https://numalariamodeling.github.io/snt-code-python-french-version/#shapefiles';">View py FR</button>
             </div>
             
             <h4 id="stepByStep">Step-by-step guide</h4>
